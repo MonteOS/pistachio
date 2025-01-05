43e3f95895f0dad5acd2cc9ad5a10846b769f4af
Bob Nicholson <bobniklv@gmail.com>
2025-01-05 17:18:29 +0300

    :sparkles: add base hilt integration

--- app/src/main/java/com/yawl/os/pistachio/Activity.kt
+++ app/src/main/java/com/yawl/os/pistachio/Activity.kt
@@ -3,6 +3,7 @@ package com.yawl.os.pistachio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -14,8 +15,13 @@ import com.yawl.os.pistachio.sim.SimInfo
 import com.yawl.os.pistachio.system.SystemInfo
 import com.yawl.os.pistachio.ui.HeaderWithPairs
 import com.yawl.os.pistachio.ui.ListWithHeaders
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class Activity : ComponentActivity() {
+
+    private val viewModel: AppViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ app/src/main/java/com/yawl/os/pistachio/App.kt
@@ -0,0 +1,7 @@
+package com.yawl.os.pistachio
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
