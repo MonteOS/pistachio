fb402d933ae36f7957fe10d318141a128cc16d2e
Bob Nicholson <bobniklv@gmail.com>
2025-01-05 16:54:19 +0300

    :sparkles: update list ui

--- app/src/main/java/com/yawl/os/pistachio/Activity.kt
+++ app/src/main/java/com/yawl/os/pistachio/Activity.kt
@@ -12,7 +12,8 @@ import com.yawl.os.pistachio.drm.DrmInfo
 import com.yawl.os.pistachio.screen.ScreenInfo
 import com.yawl.os.pistachio.sim.SimInfo
 import com.yawl.os.pistachio.system.SystemInfo
-import com.yawl.os.pistachio.ui.List
+import com.yawl.os.pistachio.ui.HeaderWithPairs
+import com.yawl.os.pistachio.ui.ListWithHeaders
 
 class Activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,45 +24,63 @@ class Activity : ComponentActivity() {
                 val general = SystemInfo(property)
                 val screen = ScreenInfo(this)
                 val appScope = AppScopeInfo(this)
-                val sim = SimInfo(this)
+                val sim = SimInfo(this).sim().first()
                 val drm = DrmInfo()
-                val pairs = listOf(
-                    "DRM ID" to drm.drmId(),
-                    "SIM" to sim.sim().toString(),
-                    "Android ID" to appScope.androidId(),
-                    "Resolution" to screen.resolution(),
-                    "Ratio" to screen.ratio(),
-                    "Density" to screen.density(),
-                    "X DPI" to screen.xDPI(),
-                    "Y DPI" to screen.yDPI(),
-                    "Refresh rate" to screen.refreshRate(),
-                    "Modes" to screen.modes(),
-                    "Manufacturer" to general.manufacturer(),
-                    "Brand" to general.brand(),
-                    "Model" to general.model(),
-                    "Release" to general.release(),
-                    "API" to general.api(),
-                    "Device" to general.device(),
-                    "Product" to general.product(),
-                    "Board" to general.board(),
-                    "Platform" to general.platform(),
-                    "Build" to general.build(),
-                    "Java VM" to general.javaVM(),
-                    "Security" to general.security(),
-                    "Kernel" to general.kernelVersion(),
-                    "Baseband" to general.baseband(),
-                    "Build Type" to general.buildType(),
-                    "Tags" to general.tags(),
-                    "Incremental" to general.incremental(),
-                    "Description" to general.description(),
-                    "Fingerprint" to general.fingerprint(),
-                    "Build Date" to general.buildDate(),
-                    "Builder" to general.builder(),
-                    "Language" to general.language(),
-                    "Timezone" to general.timezone()
+                val list = listOf(
+                    HeaderWithPairs(
+                        header = "Device Identifiers",
+                        data = listOf(
+                            "DRM ID" to drm.drmId(),
+                            "Android ID" to appScope.androidId(),
+                        )
+                    ),
+                    HeaderWithPairs(
+                        header = "SIM Identifiers",
+                        data = listOf(
+                            "SIM operator name" to sim.operatorName,
+                            "SIM operator code" to sim.operatorCode,
+                            "SIM Country" to sim.country,
+                            "SIM Country Code" to sim.countryCode,
+                        )
+                    ),
+                    HeaderWithPairs(
+                        header = "Device Specifications",
+                        data = listOf(
+                            "Resolution" to screen.resolution(),
+                            "Ratio" to screen.ratio(),
+                            "Density" to screen.density(),
+                            "X DPI" to screen.xDPI(),
+                            "Y DPI" to screen.yDPI(),
+                            "Refresh rate" to screen.refreshRate(),
+                            "Modes" to screen.modes(),
+                            "Manufacturer" to general.manufacturer(),
+                            "Brand" to general.brand(),
+                            "Model" to general.model(),
+                            "Release" to general.release(),
+                            "API" to general.api(),
+                            "Device" to general.device(),
+                            "Product" to general.product(),
+                            "Board" to general.board(),
+                            "Platform" to general.platform(),
+                            "Build" to general.build(),
+                            "Java VM" to general.javaVM(),
+                            "Security" to general.security(),
+                            "Kernel" to general.kernelVersion(),
+                            "Baseband" to general.baseband(),
+                            "Build Type" to general.buildType(),
+                            "Tags" to general.tags(),
+                            "Incremental" to general.incremental(),
+                            "Description" to general.description(),
+                            "Fingerprint" to general.fingerprint(),
+                            "Build Date" to general.buildDate(),
+                            "Builder" to general.builder(),
+                            "Language" to general.language(),
+                            "Timezone" to general.timezone()
+                        )
+                    )
                 )
-                List(
-                    pairs = pairs,
+                ListWithHeaders(
+                    sections = list,
                     modifier = Modifier.padding(
                         innerPadding
                     )
--- app/src/main/java/com/yawl/os/pistachio/ui/List.kt
+++ app/src/main/java/com/yawl/os/pistachio/ui/List.kt
@@ -6,52 +6,88 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun List(
-    pairs: List<Pair<String, String>>,
+fun ListWithHeaders(
+    sections: List<HeaderWithPairs>,
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        items(pairs.size) { index ->
-            val backgroundColor = if (index % 2 == 0) {
-                Color.LightGray
-            } else {
-                Color.White
+        sections.forEach { section ->
+            item {
+                HorizontalDivider(
+                    color = MaterialTheme.colorScheme.onBackground.copy(
+                        alpha = 0.5f
+                    )
+                )
+                Text(
+                    text = section.header,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = MaterialTheme.colorScheme.primaryContainer
+                        )
+                        .padding(vertical = 12.dp, horizontal = 16.dp)
+                )
+                HorizontalDivider(
+                    color = MaterialTheme.colorScheme.onBackground.copy(
+                        alpha = 0.5f
+                    )
+                )
             }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(backgroundColor)
-                    .padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = pairs[index].first,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal,
-                    modifier = Modifier.weight(1f)
-                )
-                Text(
-                    text = pairs[index].second,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal,
-                    modifier = Modifier.weight(1f),
-                    textAlign = TextAlign.End
-                )
+            items(section.data.size) { index ->
+                val pair = section.data[index]
+                val backgroundColor = if (index % 2 == 0) {
+                    MaterialTheme.colorScheme.surfaceVariant
+                } else {
+                    MaterialTheme.colorScheme.surface
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(backgroundColor)
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = pair.first,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Text(
+                        text = pair.second,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.weight(1f),
+                        textAlign = TextAlign.End
+                    )
+                }
             }
         }
     }
 }
+
+data class HeaderWithPairs(
+    val header: String,
+    val data: List<Pair<String, String>>
+)
