57af2c1599dff94d4f8f377351bd70b4d8d97e5d
Bob Nicholson <bobniklv@gmail.com>
2025-01-17 15:08:48 +0300

    :sparkles: add simple dialog if value is too long

--- app/src/main/java/com/monte/os/pistachio/ui/List.kt
+++ app/src/main/java/com/monte/os/pistachio/ui/List.kt
@@ -1,15 +1,22 @@
 package com.monte.os.pistachio.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -22,6 +29,10 @@ fun ListWithHeaders(
     sections: List<HeaderWithPairs>,
     modifier: Modifier = Modifier
 ) {
+    var showDialog by remember { mutableStateOf(false) }
+    var dialogHeader by remember { mutableStateOf("") }
+    var dialogText by remember { mutableStateOf("") }
+
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
@@ -73,17 +84,75 @@ fun ListWithHeaders(
                         color = MaterialTheme.colorScheme.onSurface,
                         modifier = Modifier.weight(1f)
                     )
-                    Text(
-                        text = pair.second,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Medium,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        modifier = Modifier.weight(1f),
-                        textAlign = TextAlign.End
-                    )
+                    if (pair.second.length > 100) {
+                        Text(
+                            text = "...",
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier
+                                .weight(1f)
+                                .clickable {
+                                    dialogHeader = pair.first
+                                    dialogText = pair.second
+                                    showDialog = true
+                                },
+                            textAlign = TextAlign.End
+                        )
+                    } else {
+                        Text(
+                            text = pair.second,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.End
+                        )
+                    }
                 }
             }
         }
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = dialogHeader) },
+            text = {
+                LazyColumn {
+                    item {
+                        Text(text = dialogText)
+                    }
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
+}
+
+interface Element {
+
+    val name: String
+
+    fun data(): String
+
+    data class StringBased(
+        override val name: String,
+        private val value: String
+    ) : Element {
+        override fun data() = value
+    }
+
+    data class ListBased(
+        override val name: String,
+        private val values: List<String>
+    ) : Element {
+        override fun data() = values
+            .joinToString("\n")
     }
 }
 
