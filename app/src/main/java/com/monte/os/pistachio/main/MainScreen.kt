00288035ac8949a69c31b7f88e00a9cdcad17e86
Bob Nicholson <bobniklv@gmail.com>
2025-01-23 23:08:06 +0300

    :sparkles: refactor

--- app/src/main/java/com/monte/os/pistachio/main/MainScreen.kt
+++ app/src/main/java/com/monte/os/pistachio/main/MainScreen.kt
@@ -1,12 +1,22 @@
 package com.monte.os.pistachio.main
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.monte.os.designsystem.text.JustText
+import com.monte.os.designsystem.text.SingleText
+import com.monte.os.pistachio.R
 import com.monte.os.pistachio.main.component.item.ItemsDialog
 import com.monte.os.pistachio.main.component.section.Section
 import com.monte.os.pistachio.main.component.section.SectionsList
@@ -20,14 +30,18 @@ fun MainScreen(
         mutableStateOf<Int?>(null)
     }
 
-    SectionsList(
+    Column(
         modifier = modifier
             .fillMaxSize(),
-        list = list,
-        onClick = { index ->
-            selected = index
-        }
-    )
+    ) {
+        Header()
+        SectionsList(
+            list = list,
+            onClick = { index ->
+                selected = index
+            }
+        )
+    }
 
     selected?.let { index ->
         val selectedSection = list[index]
@@ -35,6 +49,30 @@ fun MainScreen(
             title = selectedSection.title,
             items = selectedSection.items,
             onDismiss = { selected = null }
+        )
+    }
+}
+
+@Composable
+fun Header(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(
+                vertical = 16.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SingleText(
+            text = JustText(
+                text = stringResource(
+                    id = R.string.monteos_privacy_check
+                ),
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
         )
     }
 }
