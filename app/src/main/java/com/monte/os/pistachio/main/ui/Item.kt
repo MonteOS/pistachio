873bb043f09a079effdb69d5fa39f908c8adbb29
Bob Nicholson <bobniklv@gmail.com>
2025-01-23 19:09:25 +0300

    :sparkles: refactor

--- app/src/main/java/com/monte/os/pistachio/main/ui/Item.kt
+++ app/src/main/java/com/monte/os/pistachio/main/ui/Item.kt
@@ -1,12 +1,9 @@
 package com.monte.os.pistachio.main.ui
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.monte.os.pistachio.design.text.JustText
 import com.monte.os.pistachio.design.text.SingleText
