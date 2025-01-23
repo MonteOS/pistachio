2a9bfa6d5da1796c76b6fd73545be777ceffe220
Bob Nicholson <bobniklv@gmail.com>
2025-01-23 20:15:23 +0300

    :sparkles: refactor

--- app/src/main/java/com/monte/os/pistachio/main/ui/SectionUi.kt
+++ app/src/main/java/com/monte/os/pistachio/main/ui/SectionUi.kt
@@ -3,9 +3,11 @@ package com.monte.os.pistachio.main.ui
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -21,7 +23,9 @@ fun SectionUi(
 ) {
     Row(
         modifier = modifier
-            .clickable { onClick() }
+            .fillMaxWidth()
+            .clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             painter = painterResource(
