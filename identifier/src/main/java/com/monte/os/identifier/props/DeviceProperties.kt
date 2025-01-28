25499b45872582cbe3901ac9b9b2dee613645e69
Bob Nicholson <bobniklv@gmail.com>
2025-01-28 17:34:45 +0300

    :sparkles: Add sim network type getting without any permission

--- identifier/src/main/java/com/monte/os/identifier/props/DeviceProperties.kt
+++ identifier/src/main/java/com/monte/os/identifier/props/DeviceProperties.kt
@@ -2,17 +2,12 @@ package com.monte.os.identifier.props
 
 import com.monte.os.identifier.Item
 import com.monte.os.identifier.ProvideIdentifiers
-import com.topjohnwu.superuser.Shell
 import javax.inject.Inject
 
 class DeviceProperties @Inject constructor(
 ) : ProvideIdentifiers {
     override fun provide(): List<Item> {
-        return Shell.cmd(
-            "/system/bin/getprop"
-        )
-            .exec()
-            .out
+        return Property.all()
             .mapNotNull { line ->
                 val regex = """\[(.+?)]: \[(.*?)]""".toRegex()
                 val matchResult = regex.find(line)
