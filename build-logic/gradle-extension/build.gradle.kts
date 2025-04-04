08415fd1c872f7018a4fbcce885837971456e95b
Bob Nicholson <bobniklv@gmail.com>
2025-04-04 18:38:26 +0300

    :art: Use api for gradle libs workaround

--- build-logic/gradle-extension/build.gradle.kts
+++ build-logic/gradle-extension/build.gradle.kts
@@ -8,5 +8,14 @@ group = "com.android.build-logic"
 dependencies {
     compileOnly(gradleApi())
     // workaround for https://github.com/gradle/gradle/issues/15383
-    implementation(files((libs as Any).javaClass.superclass.protectionDomain.codeSource.location))
+    api(
+        files(
+            (libs as Any)
+                .javaClass
+                .superclass
+                .protectionDomain
+                .codeSource
+                .location
+        )
+    )
 }
