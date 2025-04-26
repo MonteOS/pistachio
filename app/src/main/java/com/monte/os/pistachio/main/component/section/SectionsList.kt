1e027a982e4ca034889fb641620d99936c34e684
Bob Nicholson <bobniklv@gmail.com>
2025-04-26 16:41:25 +0300

    :tada: Add border for first (main) section

--- app/src/main/java/com/monte/os/pistachio/main/component/section/SectionsList.kt
+++ app/src/main/java/com/monte/os/pistachio/main/component/section/SectionsList.kt
@@ -1,9 +1,12 @@
 package com.monte.os.pistachio.main.component.section
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SectionsList(
@@ -19,6 +22,7 @@ fun SectionsList(
             item {
                 SectionUi(
                     section = section,
+                    withBorder = index == 0,
                     onClick = { onClick(index) }
                 )
             }
